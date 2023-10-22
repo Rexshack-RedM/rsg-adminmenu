ecf6c4d6eb91c52a802ca098cc26a29ae1e5c03a
RexshackGaming <rexshackuk@gmail.com>
2023-10-22 17:39:35 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,27 @@
+fx_version 'cerulean'
+lua54 'yes'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rsg-adminmenu'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    '@rsg-core/shared/locale.lua',
+    'locales/en.lua', -- preferred language
+    'config.lua',
+}
+
+client_scripts {
+    'client/client.lua'
+}
+
+server_scripts {
+    'server/server.lua'
+}
+
+dependencies {
+    'rsg-core',
+    'ox_lib',
+}
---
+++ locales/en.lua
@@ -0,0 +1,9 @@
+local Translations = {
+    lang1 = 'Example Text',
+    lang2 = 'Example Text',
+}
+
+Lang = Locale:new({
+    phrases = Translations,
+    warnOnMissing = true
+})
